a1e73c76e91d829e9f710efcbad2564cb37cbddb
Julia <julialopezcalvo95@gmail.com>
2025-02-05 11:13:16 +0100

    incorporar json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,32 +2,49 @@ import '../styles/App.scss';
 import logoOwen from '../images/logo-owen.png'
 
 
-
-
-
 function App() {
   return (
-    <div className='header'>
-      <img className="img" src={logoOwen} alt="logo owen" />
-      <h1 className='header__h1'>Owen Wilson</h1>
-      <p className='header__wow'> WOW</p>
+    <div>
+      <header className='header'>
+        <img className="img" src={logoOwen} alt="logo owen" />
+        <h1 className='header__h1'>Owen Wilson</h1>
+        <p className='header__wow'> WOW</p>
 
-      <p className='header__paragraph'>Busca la pelicula de Owen Wilson donde m'as veces dice "Wow". Puedes filtrar tanto por película como por año 😉</p>
+        <p className='header__paragraph'>Busca la pelicula de Owen Wilson donde m'as veces dice "Wow". Puedes filtrar tanto por película como por año 😉</p>
 
-      <section className='header__filter'>
+      </header>
 
-        <ul className='header__ul'>
-          <li className='header__li'>
-            <label className="header__label" htmlFor="">Movie</label>
-            <input className="header__input" type="text" />
-          </li>
+      <main>
+        <section className='header__filter'>
 
-          <li className='header__li'> <label className="header__label" htmlFor="">Year</label>
-            <input className="header__input" type="text" />
-          </li>
-        </ul>
+          <ul className='header__ul'>
+            <li className='header__li'>
+              <label className="header__label" htmlFor="">Movie</label>
+              <input className="header__input" type="text" />
+            </li>
 
-      </section>
+            <li className='header__li'> <label className="header__label" htmlFor="">Year</label>
+              <input className="header__input" type="text" />
+            </li>
+          </ul>
+
+        </section>
+
+        <div className='listado'>
+          <ul className='listado__ul'>
+            <li>
+              <img src="" alt="" />
+              <h3></h3>
+              <p></p>
+              <p></p>
+            </li>
+          </ul>
+
+
+        </div>
+      </main>
+
+
 
 
     </div>
