d06c77a77b4e7b262e8b856d92ad2c8d6460987a
Julia <julialopezcalvo95@gmail.com>
2025-02-05 10:58:23 +0100

    estilos a la página de login

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,35 @@
 import '../styles/App.scss';
+import logoOwen from '../images/logo-owen.png'
+
+
+
+
 
 function App() {
   return (
-    <div>
-      <h1>Owen Wilson "Wow"</h1>   
-      <section>
+    <div className='header'>
+      <img className="img" src={logoOwen} alt="logo owen" />
+      <h1 className='header__h1'>Owen Wilson</h1>
+      <p className='header__wow'> WOW</p>
 
-        <label htmlFor="">Movie</label>
-        <input type="text" />
+      <p className='header__paragraph'>Busca la pelicula de Owen Wilson donde m'as veces dice "Wow". Puedes filtrar tanto por película como por año 😉</p>
 
+      <section className='header__filter'>
 
-        <label htmlFor="">Year</label>
-        <input type="text" />
+        <ul className='header__ul'>
+          <li className='header__li'>
+            <label className="header__label" htmlFor="">Movie</label>
+            <input className="header__input" type="text" />
+          </li>
+
+          <li className='header__li'> <label className="header__label" htmlFor="">Year</label>
+            <input className="header__input" type="text" />
+          </li>
+        </ul>
+
       </section>
 
-      
+
     </div>
   )
 }
