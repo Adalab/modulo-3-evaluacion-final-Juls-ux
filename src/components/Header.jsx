4049026ca3dc2534ff6521618adff8231722da23
Julia <julialopezcalvo95@gmail.com>
2025-02-06 14:01:16 +0100

    add audio y director/a

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import logoOwen from '../images/logo-owen.png'
+import { Link } from 'react-router';
 
 function Header ()
 
 {
     return(
         <header className='header'>
-        <img className="img" src={logoOwen} alt="logo owen" />
+        <Link to="/">
+    <img className="img" src={logoOwen} alt="logo owen" />
+</Link>
         <h1 className='header__h1'>Owen Wilson</h1>
         <p className='header__wow'> WOW</p>
 
--- src/components/pages/MovieSceneDetail.jsx
+++ src/components/pages/MovieSceneDetail.jsx
@@ -11,15 +11,17 @@ function MovieSceneDetail({ findPeli }) {
         return <p className='listado__no-result'>No se han encontrado resultados 😥 
                     <img className="listado__img-noResult" src={owenSoprendido} alt="" /></p>
     }return (
-        <section>
-            <h2>Información de {pelicula.movie}</h2>
+        <section className='detailSection'>
+            <h2 className='detailSection__h2'>Información de {pelicula.movie}</h2>
             <img className="listado__img" src={pelicula.poster} alt={pelicula.movie} />
             <h3 className='listado__h3'>{pelicula.movie}</h3>
             <h4 className='listado__h4'>{pelicula.full_line}</h4>
+            <p className='listado__paragraphYear'>Director/a: {pelicula.director}</p>
             <p className='listado__paragraphYear'>{pelicula.year}</p>
             <p className='listado__paragraph'>{pelicula.timestamp}</p>
+            <a className='listado__link-paragraph' href={pelicula.audio}>🎧</a>
 
-            <Link to="/">Volver</Link>
+            <Link  className='detailSection__btn' to="/">Volver</Link>
         </section>
     );
 }
